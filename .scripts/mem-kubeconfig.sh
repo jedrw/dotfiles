69fe8c7476714ff7d36878ab069e669dc8bc7d39
lupinelab <jed@lupinelab.co.uk>
2024-05-01 01:38:16 +0100

    changes to dotfiles

--- .scripts/mem-kubeconfig.sh
+++ .scripts/mem-kubeconfig.sh
@@ -5,5 +5,6 @@ mkdir -p /tmp/kube
 echo "Created /tmp/kube dir"
 
 doppler --project jedrw secrets get KUBECONFIG_DATA --plain | base64 -d > $MEM_KUBECONFIG
+chmod 600 $MEM_KUBECONFIG
 echo "Wrote kubeconfig into $MEM_KUBECONFIG"
 
